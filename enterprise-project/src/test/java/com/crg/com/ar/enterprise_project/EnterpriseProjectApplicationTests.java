05d2cf45e66a601397e87e22a0576d876048035b
Claudio R. González <claudio.gonzalez@engen.com.mx>
2025-03-11 17:53:53 -0300

    feat: First main commit

---
+++ enterprise-project/src/test/java/com/crg/com/ar/enterprise_project/EnterpriseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crg.com.ar.enterprise_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
